11004c1b6a61ab5917cc548bea7b2e945f2a2c3e
Gilles30 <lorison.gilles@gmail.com>
2022-03-07 21:50:48 -0300

    🔥 Finishing API and deploy

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+
+const port = process.env.PORT || 8877
+
+app.get('/', (req, res) => {
+  res.send('Hellow how are you?')
+})
+
+app.get('/data', (req, res) => {
+  res.sendFile(__dirname + '/src/server/data.json')
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
