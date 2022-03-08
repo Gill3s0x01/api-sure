023d97eb94eaf5c425050eb9c9ba56e7f668148e
Gilles30 <lorison.gilles@gmail.com>
2022-03-07 23:12:47 -0300

    🐛 fixed error cors

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
-
+const cors = require('cors')
 const app = express()
 
 const port = process.env.PORT || 8877
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.send('Hellow how are you?')
